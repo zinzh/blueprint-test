499aa2d9d98fcab8c4017b9f7d69eb40eabc82dc
zinzh <gilbert.y.sassine@gmail.com>
2025-04-12 01:51:35 +0300

    init commit

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "create_vpc" {
+  description = "Controls if VPC should be created (it affects almost all resources)"
+  type        = bool
+  default     = true
+}
+
+variable "name" {
+  description = "Name to be used on all the resources as identifier"
+  type        = string
+  default     = ""
+}
+
+variable "cidr" {
+  description = "(Optional) The IPv4 CIDR block for the VPC. CIDR can be explicitly set or it can be derived from IPAM using `ipv4_netmask_length` & `ipv4_ipam_pool_id`"
+  type        = string
+  default     = "10.0.0.0/16"
+}
